5c58456b655eba979a2a20066d96f22e45fce8b5
Keso0420 <vicho.gonzalez1024@gmail.com>
2026-06-15 01:31:30 -0400

    backup 1

---
+++ medicheck/src/app/components/auth.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/interfaces'; 
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private API_URL = 'http://localhost:3000/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  register(userData: User): Observable<any> {
+    return this.http.post(`${this.API_URL}/register`, userData);
+  }
+
+  login(credentials: Pick<User, 'email' | 'password'>): Observable<any> {
+    return this.http.post(`${this.API_URL}/login`, credentials);
+  }
+
+  forgotPassword(email: string): Observable<any> {
+    return this.http.post(`${this.API_URL}/forgot-password`, { email });
+  }
+}
